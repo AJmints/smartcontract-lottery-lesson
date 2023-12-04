8b5671c27c1e5d8955801fac1656333f1ec39de5
ajmints <alexjmints@gmail.com>
2023-12-04 16:19:49 -0600

    Starting to explore chainlink keepers

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -2,22 +2,39 @@
 
 pragma solidity ^0.8.7;
 
-error Raffle__NotEnoughEthEntered();
+import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
-contract Raffle {
+error Raffle__NotEnoughEthEntered();
+error Raffle__TransferFailed();
+
+contract Raffle is VRFConsumerBaseV2 {
 
     /* State Variables */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATION = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    /* Lottery Variables */
+    address private s_recentWinner;
 
     /* Events */
     /* Convention is to name the event the function name reversed */
-    event RaffleEnter(
-        address indexed player
-    );
+    event RaffleEnter( address indexed player );
+    event RequestRaffleWinner( uint256 indexed requestId );
+    event WinnerPicked(address indexed winner);
 
-    constructor(uint256 entranceFee) {
+    constructor(address vrfCoordinaterV2, uint256 entranceFee, bytes32 gasLane, uint64 subscriptionId, uint32 callbackGasLimit) VRFConsumerBaseV2(vrfCoordinaterV2){
         i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinaterV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() payable public {
@@ -29,14 +46,40 @@ contract Raffle {
     }
 
 
-    // function pickRandomWinner() {}
+    function requestRandomWinner() external {
+        /* 2 transaction process - good to prevent brute force attempts and keeps the final answer random  // View notes to see the requestRandomWords() outline */
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATION,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        emit RequestRaffleWinner(requestId);
+    }
 
+    function fulfillRandomWords(uint256 /*requestId*/, uint256[] memory randomWords) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if(!success) {
+            revert Raffle__TransferFailed();
+        }
+        emit WinnerPicked(recentWinner);
+    }
+
+    /* View / Pure functions */
     function getEntranceFee() public view returns(uint256) {
         return i_entranceFee;
     }
 
     function getPlayer(uint256 index) public view returns (address) {
         return s_players[index];
+    }
+
+    function getRecentWinner() public view returns(address) {
+        return s_recentWinner;
     }
 }
 
